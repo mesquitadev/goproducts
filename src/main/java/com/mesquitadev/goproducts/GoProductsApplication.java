771553858bf0d5a4dd0c91345a0dab367eb055bb
Victor Mesquita <mesquitadev@gmail.com>
2022-03-10 18:06:04 -0300

    M - Initial

---
+++ src/main/java/com/mesquitadev/goproducts/GoProductsApplication.java
@@ -0,0 +1,11 @@
+package com.mesquitadev.goproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoProductsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GoProductsApplication.class, args);
+    }
+}
